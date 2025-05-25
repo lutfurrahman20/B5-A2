3df9484fa19461ce01fe3a7ef84b1345030fdb86
lutfur rahman siddiquee <121648644+lutfurrahman20@users.noreply.github.com>
2025-05-25 16:30:05 +0600

    problem solve added

---
+++ PostgreSQL_Assignment_Answer.sql
@@ -0,0 +1,68 @@
+
+-- problem 01
+INSERT INTO rangers(name, region) VALUES
+('Derek Fox', 'Coastal Plains');
+
+SELECT * FROM rangers;
+
+-- problem 02
+
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
+FROM sightings;
+
+-- problem 03
+
+SELECT * FROM sightings
+WHERE location ILIKE '%Pass%';
+
+
+-- problem 04
+
+SELECT name,
+  (
+    SELECT COUNT(*) 
+    FROM sightings s 
+    WHERE s.ranger_id = r.ranger_id
+  ) 
+  AS total_sightings
+FROM rangers r;
+
+-- problem 05
+
+SELECT common_name FROM species
+WHERE species_id NOT IN (
+    SELECT species_id FROM sightings
+);
+
+-- problem 6
+
+SELECT common_name, sighting_time, name
+FROM species, sightings, rangers
+WHERE species.species_id = sightings.species_id
+  AND rangers.ranger_id = sightings.ranger_id
+ORDER BY sighting_time DESC
+LIMIT 2;
+
+-- problem 7
+
+UPDATE species
+SET conservation_status = 'Historic'
+WHERE EXTRACT(YEAR FROM discovery_date) < 1800;
+
+
+
+-- problem 8
+
+
+
+
+-- problem 9
+
+DELETE FROM rangers
+WHERE ranger_id NOT IN (
+    SELECT DISTINCT ranger_id FROM sightings
+);
+
+
+
+
